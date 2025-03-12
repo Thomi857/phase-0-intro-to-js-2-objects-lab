02f0baace5e7e5883773253c67af29460924b5e4
CliffLinusthomi <linusthomi2@gmail.com>
2025-03-12 10:40:14 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name: "Sam",
+      streetAddress: "11 Broadway"
+      };
+
+function updateEmployeeWithKeyAndValue(employee, key ,value){
+    return {...employee, [key]: value};
+        return;
+    };
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key ,value){
+    employee[key] = value;  // Directly mutate the original object
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = { ...employee };  // Correctly create a copy of the object
+    delete newEmployee[key];  // Delete the key from the copied object
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];  // Directly mutate the original object
+    return employee;
+}
